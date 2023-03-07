10b683030e300cfa88db0f344e3aa09ddf3dbb91
Tino <tino5280@gmail.com>
2023-03-06 21:43:35 -0700

    fin

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(games) {
+    const wonGame = games.find((game) => {
+        return game.result === "W" ;
+
+    })
+    if (wonGame) {
+        return wonGame.year;
+    }
+
+}
